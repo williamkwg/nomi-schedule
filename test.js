76c67b47072142bbb2eba4369b87bec63e94b606
weiguo.kong <weiguo.kong@qunar.com>
2018-08-03 17:41:13 +0800

    优化

--- test.js
+++ test.js
@@ -7,5 +7,11 @@ let app = {
 const sLoader = new ScheduleLoader(['test/schedule'], app);
 
 setTimeout(() => {
+  console.log(`----`)
+  sLoader.runSchedule('loggerSchedule');
+  console.log(`----`)
+  sLoader.close('loggerSchedule')
+}, 3000)
+setTimeout(() => {
   sLoader.closeAll();
 }, 100000)
